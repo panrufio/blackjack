b76c51c09626ff706c6bc99a82f760650e71c859
Andrew Levine <andrew@defiant.local>
2013-06-24 23:49:13 -0400

    more cleanup and docs

--- PTPBlackJack/src/main/java/ptp/coding/blackjack/game/DealersShoe.java
+++ PTPBlackJack/src/main/java/ptp/coding/blackjack/game/DealersShoe.java
@@ -6,57 +6,86 @@ import java.util.Random;
 
 /**
  * DealersShoe is an implementation of the shoe used for Black Jack.
+ * This implements Iterator so that the shoe can hand out cards with .next().
  * 
  * @author andrew
  *
  */
 public class DealersShoe implements Iterator{
 
+	// set the default number of decks
 	protected int DS_DECKS = 1;
+
+	// value for determining when to shuffle
 	protected double DS_SHUFFLE_AT = 0.8; // 0.0 for continuous shuffle
-	//protected boolean DS_CUT = true;
-	
-	
 	
 	private Card[] DS_Shoe;
+
+	// use Random for help in the shuffle
 	private Random DS_Rand = null;
+
+	// a base deck of cards
 	protected static String[] DS_BASEDECK = {
 			"AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC",
 			"AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD",
 			"AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH",
 			"AS", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS"
 	};
-	
+
+	// index of the current card that will be dealt
 	private int DS_CurCard = 0;
 	
-	
+	/**
+	 * Basic constructor that creates the shoe and shuffles it
+	 */
 	public DealersShoe(){
 		createShoe();
 		shuffle();
 	} // end basic constructor
+
 	
+	/**
+	 * 
+	 * @param decks - sets the number of decks used in the shoe
+	 */
 	public DealersShoe(int decks){
 		DS_DECKS = decks;
 		createShoe();
 		shuffle();
 	} // end constructor
-	
+
+
+	/**
+	 * setShuffleAt() allows for a new value to be set to indicate a
+	 * shuffle needs to occur
+	 * 
+	 * @param d - value to indicate that shuffle needs to be done
+	 */
 	public void setShuffleAt(double d){
 		if(d >= 0 && d <= 1){
 			DS_SHUFFLE_AT = d;
 		}
-	}
+	} // end setShuffleAt
 	
 
+	/**
+	 * createShoe() with produce the array of all the cards in the shoe
+	 */
 	private void createShoe(){
+		
+		// allocate the space needed for the cards
 		DS_Shoe = new Card[DS_DECKS * DS_BASEDECK.length];
+
+		// go through multiple decks and assign the card values
 		for(int x = 0; x < DS_DECKS; x++){
 			for(int y = 0; y < DS_BASEDECK.length; y++){
 				int pos = x * DS_BASEDECK.length + y;
 				DS_Shoe[pos] = new Card(DS_BASEDECK[y]);
 			}
 		}
+		
 	} // end createShoe
+	
 	
 	/**
 	 * Shuffling is a run through the array of all cards and swap positions
@@ -66,6 +95,7 @@ public class DealersShoe implements Iterator{
 			DS_Rand = new Random(System.currentTimeMillis());
 		}
 		
+		// touch every position in the deck once
 		for(int x = 0; x < DS_Shoe.length; x++){
 			int p1 = DS_Rand.nextInt(DS_Shoe.length);
 			int p2 = DS_Rand.nextInt(DS_Shoe.length);
@@ -75,49 +105,79 @@ public class DealersShoe implements Iterator{
 				DS_Shoe[p2] = tmp;				
 			}
 		}
+		
+		// set the pointer of the next card at the top
 		DS_CurCard = 0;
+		
 	} // end shuffle
+
 	
 	public int getDealtCount(){
 		return DS_CurCard;
 	} // end getDepth
+
+	
+	/**
+	 * 
+	 * @return - how much of the shoe has been used
+	 */
 	public double getDepth(){
 		double retVal = (double) DS_CurCard / (double) DS_Shoe.length;
 		return retVal;
 	} // end getDepth
 	
+
+	/**
+	 * 
+	 * @return - true if the usage of the cards is beyond the threshold of where to shuffle
+	 */
 	public boolean shouldShuffle(){
+
+		// check if we are at the top of the shoe
 		if(DS_CurCard == 0){
 			return false;
 		}
+		
+		// evaluate the depth
 		if(getDepth() >= DS_SHUFFLE_AT){
 			return true;
 		} else {
 			return false;
-		}		
-	}
+		}
+		
+	} // end shouldShuffle
+
 	
 	public int getNumberCards(){
 		return DS_Shoe.length;
 	}
-	
+
+	/**
+	 * the show always has more cards - it is BlackJack
+	 */
 	public boolean hasNext(){
 		return true;
 	} // end hasNext
+
+	
 	public void remove(){} // end remove
+	
+	
+	/**
+	 * hand out cards
+	 */
 	public Card next(){
+		
+		// shuffle if at the end
 		if(DS_CurCard >= DS_Shoe.length){
 			shuffle();
 			DS_CurCard = 0;
 		}
+		
 		Card retCard = DS_Shoe[DS_CurCard];
 		DS_CurCard++;
 		return retCard;
 		
 	} // end next
-	
-	
-	
-	
 	
 } // end DealersShow
--- PTPBlackJack/src/main/java/ptp/coding/blackjack/game/PlayersHand.java
+++ PTPBlackJack/src/main/java/ptp/coding/blackjack/game/PlayersHand.java
@@ -2,40 +2,88 @@ package ptp.coding.blackjack.game;
 
 import java.util.ArrayList;
 
+/**
+ * PlayersHand is a class that serves to keep track of the card of a black jack hand.
+ * 
+ * @author andrew
+ *
+ */
 public class PlayersHand {
 
+	// the cards of the hand
 	protected ArrayList<Card> PH_Hand;
+
 	protected boolean PH_isDealer = false;
 	protected boolean PH_isBusted = false;
+	
+	// count of aces in the hand
 	protected int PH_aceCnt = 0;
+
+	// is this a soft count
 	protected boolean PH_isSoft = false;
+
+	// best hand
 	protected int PH_BEST = 21;
 	
-	public PlayersHand(){
-		PH_Hand = new ArrayList<Card>();
-		
-	}
 	
+	/**
+	 * basic constructor
+	 */
+	public PlayersHand(){
+		PH_Hand = new ArrayList<Card>();		
+	} // end constructor
+
+	
+	/**
+	 * Dealear constructor
+	 * 
+	 * @param b - true for setting this hand as the hand of the dealer
+	 */
 	public PlayersHand(boolean b){
 		PH_Hand = new ArrayList<Card>();
 		PH_isDealer = b;
-	} // end basic constructor
+	} // end constructor
 
+	
+	/**
+	 * Constructor for player with an initial card
+	 * 
+	 * @param c - first card in the hand
+	 */
 	public PlayersHand(Card c){
 		PH_Hand = new ArrayList<Card>();
 		PH_Hand.add(c);
-	}
+	} // end constructor
 	
+
+	/**
+	 * Dealer constructor with card
+	 * 
+	 * @param b - true for setting this hand as the hand of the dealer
+	 * @param c - first card in the hand
+	 */
 	public PlayersHand(boolean b, Card c){
 		PH_isDealer = b;
 		PH_Hand = new ArrayList<Card>();
 		PH_Hand.add(c);
 	}
 
+	/**
+	 * addCard will add a card to the hand
+	 * 
+	 * @param c - card to add
+	 */
 	public void addCard(Card c){
 		PH_Hand.add(c);
-	}
+	} // end addCard
 	
+
+	/**
+	 * showing with display the cards showing for the hand.
+	 * If this is a dealers hand, then the down card shows as XX
+	 * 
+	 * @return the sum of the cards or the value of the one card for the dealer
+	 */
 	public int showing(){
 		if(PH_Hand.size() == 0){
 			return 0;
@@ -51,11 +99,22 @@ public class PlayersHand {
 		}
 	} // end showing
 	
+	
+	/**
+	 * getHandValue() counts up the cards in the hand
+	 * 
+	 * @return the value of the hand
+	 */
 	public int getHandValue(){
+
 		int count = 0;
+
+		// reset items
 		PH_aceCnt = 0;
 		PH_isSoft = false;
 		PH_isBusted = false;
+
+		// go through the hand
 		for(int x = 0; x < PH_Hand.size(); x++){
 			if(PH_Hand.get(x).isAce()){
 				PH_aceCnt++;
@@ -63,32 +122,43 @@ public class PlayersHand {
 			count += PH_Hand.get(x).getFaceValue();
 			
 		}
+
+		// see if an ace can be used as 11
 		if(PH_aceCnt > 0 && count <= PH_BEST - 10){
 			count += 10;
 			PH_isSoft = true;
 		}
+
+		// see if we are busted
 		if(count > PH_BEST){
 			PH_isBusted = true;
 		}
 		
-		
 		return count;
+
 	} // end getHandValue
+	
 	
 	public boolean isDealer(){
 		return PH_isDealer;
 	}
+
+	
 	public boolean isPlayer(){
 		return !PH_isDealer;
 	}
+	
+	
 	public boolean isSoft(){
 		return PH_isSoft;
 		
-	} // end 
+	}
+	
 	
 	public int getNumberCards(){
 		return PH_Hand.size();
 	}
+
 	
 	public void resetHand(){
 		PH_Hand.clear();
@@ -96,15 +166,26 @@ public class PlayersHand {
 		PH_aceCnt = 0;
 		PH_isSoft = false;
 	} // end resetHand
+
 	
+	/**
+	 * showCard allows for individual cards to be shown
+	 * 
+	 * @param n - is the individual card to show
+	 * @return - the value of the card n
+	 */
 	public String showCard(int n){
 		if(n > PH_Hand.size()){
 			return "";
 		} else {
 			return PH_Hand.get(n).toString();
 		}
-	}
+	} // end showCard
+
 	
+	/**
+	 * override of toString to show the whole hand
+	 */
 	public String toString(){
 		StringBuffer sb = new StringBuffer();
 		for(int x = 0; x < PH_Hand.size(); x++){
@@ -113,7 +194,6 @@ public class PlayersHand {
 				sb.append(", ");
 			}
 		}
-		
 		return sb.toString();
 	} // end toString
 	
